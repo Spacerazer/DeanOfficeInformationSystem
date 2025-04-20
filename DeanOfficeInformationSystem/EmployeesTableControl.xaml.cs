29702b0cc20171b33b6bde0c0f052035a3702bbf
Spacerazer <e2gmodprofi@gmail.com>
2025-04-20 14:15:04 +0300

    Окно таблицы сотрудников

---
+++ DeanOfficeInformationSystem/EmployeesTableControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DeanOfficeInformationSystem
+{
+    /// <summary>
+    /// Логика взаимодействия для EmployeesTableControl.xaml
+    /// </summary>
+    public partial class EmployeesTableControl : UserControl
+    {
+        public DatabaseService.Employee SelectedEmployee { get; private set; }
+
+        public EmployeesTableControl()
+        {
+            InitializeComponent();
+        }
+
+        public void LoadData(List<DatabaseService.Employee> employee)
+        {
+            employeesDataGrid.ItemsSource = employee;
+        }
+
+        private void employeesDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SelectedEmployee = employeesDataGrid.SelectedItem as DatabaseService.Employee;
+        }
+    }
+}
